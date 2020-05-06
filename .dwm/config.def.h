3e65b3c9b2a6d364ae49d22163566e46f90b704a
Mateo Rodriguez <teorodrip@posteo.net>
2020-05-06 17:26:47 +0200

    rofi color andd ssh to dwm Mod+s

--- .dwm/config.def.h
+++ .dwm/config.def.h
@@ -59,10 +59,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "run", "-theme", "flat-orange", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *rofisshcmd[]  = { "rofi", "-show", "ssh", "-theme", "flat-orange", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = rofisshcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
