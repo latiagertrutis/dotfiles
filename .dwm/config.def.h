fd29ddfcc4612e9eb3a7fb08d7aa9da3622f2775
Mateo Rodriguez <teorodrip@posteo.net>
2020-05-06 17:05:54 +0200

    rofi theme setup

--- .dwm/config.def.h
+++ .dwm/config.def.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "run", "-theme", "flat-orange", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
