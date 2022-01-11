eab93d620e4a0235196120af27276a71f67285fb
latiagertrutis <teorodrip@gmail.com>
2022-01-11 18:48:35 +0100

    modified dwm start and added .profile

--- .dwm-bar/dwm_bar.sh
+++ .dwm-bar/dwm_bar.sh
@@ -22,28 +22,28 @@ export SEP1="["
 export SEP2="]"
 
 # Import the modules
-. "$DIR/bar-functions/dwm_countdown.sh"
-. "$DIR/bar-functions/dwm_alarm.sh"
-. "$DIR/bar-functions/dwm_transmission.sh"
-. "$DIR/bar-functions/dwm_cmus.sh"
-. "$DIR/bar-functions/dwm_mpc.sh"
+# . "$DIR/bar-functions/dwm_countdown.sh"
+# . "$DIR/bar-functions/dwm_alarm.sh"
+# . "$DIR/bar-functions/dwm_transmission.sh"
+# . "$DIR/bar-functions/dwm_cmus.sh"
+# . "$DIR/bar-functions/dwm_mpc.sh"
 . "$DIR/bar-functions/dwm_spotify.sh"
-. "$DIR/bar-functions/dwm_resources.sh"
+# . "$DIR/bar-functions/dwm_resources.sh"
 . "$DIR/bar-functions/dwm_battery.sh"
-. "$DIR/bar-functions/dwm_mail.sh"
-. "$DIR/bar-functions/dwm_backlight.sh"
-. "$DIR/bar-functions/dwm_alsa.sh"
-. "$DIR/bar-functions/dwm_pulse.sh"
-. "$DIR/bar-functions/dwm_weather.sh"
-. "$DIR/bar-functions/dwm_vpn.sh"
+# . "$DIR/bar-functions/dwm_mail.sh"
+# . "$DIR/bar-functions/dwm_backlight.sh"
+# . "$DIR/bar-functions/dwm_alsa.sh"
+# . "$DIR/bar-functions/dwm_pulse.sh"
+# . "$DIR/bar-functions/dwm_weather.sh"
+# . "$DIR/bar-functions/dwm_vpn.sh"
 . "$DIR/bar-functions/dwm_networkmanager.sh"
-. "$DIR/bar-functions/dwm_keyboard.sh"
-. "$DIR/bar-functions/dwm_ccurse.sh"
+# . "$DIR/bar-functions/dwm_keyboard.sh"
+# . "$DIR/bar-functions/dwm_ccurse.sh"
 . "$DIR/bar-functions/dwm_date.sh"
 
 # Update dwm status bar every second
 while true
 do
-    xsetroot -name "$(dwm_spotify)$(dwm_networkmanager)$(dwm_date)"
+    xsetroot -name "$(dwm_spotify)$(dwm_networkmanager)$(dwm_battery)$(dwm_date)"
     sleep 1
 done
--- .dwm/dwm-launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Status bar from https://github.com/joestandring/dwm-bar
-~/.dwm-bar/dwm_bar.sh &
-
-exec dwm
