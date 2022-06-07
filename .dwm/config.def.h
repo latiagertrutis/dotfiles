b43bed79b40ea0a98f9758cc40f4b36c75f07800
mateorodriguez <mateorodriguez@geotab.com>
2022-06-07 17:22:33 +0200

    changed color scheme to pinky

--- .dwm/config.def.h
+++ .dwm/config.def.h
@@ -1,28 +1,31 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 7;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mononoki:size=12" };
+static const char * fonts[]          = { "mononoki:size=12" };
 static const char dmenufont[]       = "mononoki:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#d99036";
-static const char *colors[][3]      = {
+static const char col_norm_border[]      = "#733543";
+static const char col_norm_foreground[]  = "#FFFFED";
+static const char col_norm_background[]  = "#853530";
+static const char col_sel_border[]      = "#B8546C";
+static const char col_sel_foreground[]  = "#FFFFED";
+static const char col_sel_background[]  = "#C44F47";
+static const char * colors[][3]      =
+{
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_norm_foreground, col_norm_background,  col_norm_border},
+	[SchemeSel]  = { col_sel_foreground, col_sel_background,  col_sel_border},
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char * tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule rules[] = {
+static const Rule rules[] =
+{
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
@@ -41,12 +44,13 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #include "tatami.c"
-static const Layout layouts[] = {
+static const Layout layouts[] =
+{
 	/* symbol     arrange function */
+	{ "[+]",      tatami },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "[+]",      tatami },
 };
 
 /* key definitions */
@@ -62,15 +66,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "rofi", "-show", "run", "-theme", "flat-orange", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *rofisshcmd[]  = { "rofi", "-show", "ssh", "-theme", "flat-orange", NULL };
+static const char * dmenucmd[] = { "rofi", "-show", "run", "-theme", "pinky", NULL };
+static const char * termcmd[]  = { "alacritty", NULL };
+static const char * rofisshcmd[]  = { "rofi", "-show", "ssh", "-theme", "pinky", NULL };
 
-static Key keys[] = {
+static Key keys[] =
+{
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = rofisshcmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY | ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -80,34 +85,37 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY | ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY | ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY | ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY | ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY | ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(XK_1,                      0)
+	TAGKEYS(XK_2,                      1)
+	TAGKEYS(XK_3,                      2)
+	TAGKEYS(XK_4,                      3)
+	TAGKEYS(XK_5,                      4)
+	TAGKEYS(XK_6,                      5)
+	TAGKEYS(XK_7,                      6)
+	TAGKEYS(XK_8,                      7)
+	TAGKEYS(XK_9,                      8)
+	{
+		MODKEY | ShiftMask,             XK_q,      quit,           {0}
+	},
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static Button buttons[] =
+{
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
